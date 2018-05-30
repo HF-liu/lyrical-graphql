4f96c0630039ce68aaa8edffebc4f8a009312b8b
David.L <liuhf24@gmail.com>
2018-05-30 11:15:02 -0700

    Setup.

--- client/index.js
+++ client/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({})
+
 
 const Root = () => {
-  return <div>Lyrical</div>
-};
+  return (
+    <AppoloProvider clien={client}>
+      <div>Lyrical</div>
+    </AppoloProvider>
+  )
+}
 
 ReactDOM.render(
   <Root />,
   document.querySelector('#root')
-);
+)
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://admin:secret123@ds239940.mlab.com:39940/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
